f62e6e54be08dc58145661eaa2e13d823e8c7f1f
hotwater <hotwater438@tutanota.com>
2019-06-01 17:40:54 +0300

    huge project clean-up

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import Network from './lib/Network'
+import Matrix from './lib/Network/lib/Matrix'
+
+let network = Network.createNetwork(
+  Network.createInLayer(
+    Matrix.createMatrix(6, 1, [[1], [0], [0], [1], [0], [0]])
+  ),
+  2,
+  3,
+  3
+)
+console.log(network)
+// console.log(network.train({ expect: [0, 1, 0] })) // TODO : check how it works
