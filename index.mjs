5a475be2a7fe20039ff0923203b786b06984c86e
hotwater <hotwater438@tutanota.com>
2019-06-08 16:37:13 +0300

    NN works, now in-depth check is needed

--- index.mjs
+++ index.mjs
@@ -1,13 +1,26 @@
 import Network from './lib/Network'
-import Matrix from './lib/Network/lib/Matrix'
 
-let network = Network.createNetwork(
-  Network.createLayer(
-    Matrix.createMatrix(6, 1, [[1], [0], [0], [1], [0], [0]])
-  ),
-  2,
-  3,
-  3
-)
-console.dir(network)
-console.log(network.train({ expect: [0, 1, 0] })) // TODO : check how it works
+let network = Network.createNetwork(2, 1, 1, 3)
+// Learning XOR ^_^
+network.train([
+  ...Array.from(Array(300), () => ({
+    data: [0, 1],
+    expect: [1]
+  })),
+  ...Array.from(Array(300), () => ({
+    data: [0, 0],
+    expect: [0]
+  })),
+  ...Array.from(Array(300), () => ({
+    data: [1, 0],
+    expect: [1]
+  })),
+  ...Array.from(Array(300), () => ({
+    data: [1, 1],
+    expect: [0]
+  }))
+])
+console.log(network)
+console.log(network.process([0, 1]))
+console.log(network.process([1, 0]))
+console.log(network.process([1, 1]))
