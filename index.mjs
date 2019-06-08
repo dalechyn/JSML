cc175ccd28f5d54ae71fa5deaf0364e469137dbe
hotwater <hotwater438@tutanota.com>
2019-06-08 17:22:41 +0300

    Getting weird output

--- index.mjs
+++ index.mjs
@@ -1,26 +1,49 @@
 import Network from './lib/Network'
 
-let network = Network.createNetwork(2, 1, 1, 3)
+function shuffle(array) {
+  let counter = array.length
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter)
+
+    // Decrease counter by 1
+    counter--
+
+    // And swap the last element with it
+    let temp = array[counter]
+    array[counter] = array[index]
+    array[index] = temp
+  }
+
+  return array
+}
+
 // Learning XOR ^_^
-network.train([
-  ...Array.from(Array(300), () => ({
+const trainData = shuffle([
+  ...Array.from(Array(2300), () => ({
     data: [0, 1],
     expect: [1]
   })),
-  ...Array.from(Array(300), () => ({
+  ...Array.from(Array(2300), () => ({
     data: [0, 0],
     expect: [0]
   })),
-  ...Array.from(Array(300), () => ({
+  ...Array.from(Array(2300), () => ({
     data: [1, 0],
     expect: [1]
   })),
-  ...Array.from(Array(300), () => ({
+  ...Array.from(Array(2300), () => ({
     data: [1, 1],
     expect: [0]
   }))
 ])
-console.log(network)
-console.log(network.process([0, 1]))
-console.log(network.process([1, 0]))
-console.log(network.process([1, 1]))
+
+let network = Network.createNetwork(2, 1, 1, 5)
+network.train(trainData)
+network.print()
+console.log('xor(0, 0) =', network.process([0, 0])[0])
+console.log('xor(0, 1) =', network.process([0, 1])[0])
+console.log('xor(1, 0) =', network.process([1, 0])[0])
+console.log('xor(1, 1) =', network.process([1, 1])[0])
